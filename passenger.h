b3f6eaed48451b79874c15c9c07f50b3b7477147
CodyCasselman <112972935+CodyCasselman@users.noreply.github.com>
2023-11-30 16:48:24 -0700

    Update passenger.h

--- passenger.h
+++ passenger.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include "flight.h"
+#include "seat.h"
 using namespace std;
 
 class Passenger {
@@ -10,20 +12,18 @@ private:
     string first_name;
     string last_name;
     string phone_number;
-    int row;
-    char column;
     int id;
+    Seat * seat;
     
 public:
     // Constructor
-    Passenger(string fname, string lname, string phone, int row, char column, int id);
+    Passenger(string fname, string lname, string phone, Seat * s, int id);
     
     // Getter functions
     string get_first_name() { return first_name; }
     string get_last_name() { return last_name; }
     string get_phone_number() { return phone_number; }
-    int get_row() { return row; }
-    char get_column() { return column; }
+    Seat * get_seat() { return seat; }
     int get_id() { return id; }
     
     // Display a single passenger information
